9cd4650eb6cee931052d18b899cb926766fa9286
Vinicius Donato <vinicaval@Viniciuss-MacBook-Pro.local>
2016-06-07 22:06:10 -0300

    initial commit

---
+++ HelloPluralsight/HelloPluralsight/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloPluralsight
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		public void OnClick(object o, EventArgs e){
+			message.Text="Goodbye Pluralsight!!";
+		}
+	}
+}
+
